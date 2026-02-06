937d717a70aba1e8e87e92f513df35d614240f74
Saroj Rai <114452296+Saroj94@users.noreply.github.com>
2026-02-06 21:11:52 +0000

    Image filtering

--- 11.cv_ImageFiltering.py
+++ 11.cv_ImageFiltering.py
@@ -1,5 +1,15 @@
 ##Image enhancer using Image filtering technique of opencv
- ##gaussian blur
+ ##gaussian blur : It is used to remove the noise and smoothen the image
 ##median blur
 ##sharpening
  
+##GAUSSIAN BLUR
+import cv2
+
+img = cv2.imread("Temi-Tea Namchi.png")
+if img is not None:
+    blurred_img = cv2.GaussianBlur(img,(9,9), 3)
+    cv2.imshow("Orginal image", img)
+    cv2.imshow("Blurred Image",blurred_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
